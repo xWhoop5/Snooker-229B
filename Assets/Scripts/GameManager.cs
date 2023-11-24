ac13e569d59ee3758ee1e0de55819d3c0777952b
9PR <nine9247lovemom@gmail.com>
2023-11-24 19:54:37 +0700

    Shoot Ball Ver. Almost Complete

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,12 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
     [SerializeField]
     private int playerScore;
+
+    public GameManager()
+    {
+    }
+
     public int PlayerScore { get { return playerScore; } set { playerScore = value;  } }
+
+    public GameObject MainCamera { get => camera; set => camera = value; }
 
     [SerializeField]
     private GameObject ballPrefab;
@@ -23,12 +31,20 @@ public class GameManager : MonoBehaviour
     [SerializeField]
     private float xInput;
 
+    [SerializeField]
+    private GameObject camera;
+
     public static GameManager Instance;
 
     // Start is called before the first frame update
     void Start()
     {
         Instance = this;
+
+        MainCamera = Camera.main.gameObject;
+        CameraBehindCueBall();
+
+
         SetBall(BallColor.Red, 1);
         SetBall(BallColor.Yellow, 2);
         SetBall(BallColor.Green, 3);
@@ -44,6 +60,8 @@ public class GameManager : MonoBehaviour
         RotateBall();
         if (Input.GetKeyDown(KeyCode.Space))
             ShootBall();
+        if (Input.GetKeyDown(KeyCode.Backspace))
+            stopBall();
     }
 
     private void SetBall(BallColor col, int i)
@@ -63,9 +81,30 @@ public class GameManager : MonoBehaviour
 
     private void ShootBall()
     {
+        camera.transform.parent = null;;
         Rigidbody rb = cueBall.GetComponent<Rigidbody>();
         rb.AddRelativeForce(Vector3.forward * 50, ForceMode.Impulse);
 
         ballLine.SetActive(false);
+    }
+
+    private void CameraBehindCueBall()
+    {
+        camera.transform.parent = cueBall.transform;
+        camera.transform.position = cueBall.transform.position
+                                    + new Vector3(0f, 7f, -10f);
+    }
+
+    private void stopBall()
+    {
+        Rigidbody rb = cueBall.GetComponent<Rigidbody>();
+        rb.velocity = Vector3.zero;
+        rb.angularVelocity = Vector3.zero;
+        cueBall.transform.eulerAngles = Vector3.zero;
+        
+        CameraBehindCueBall();
+        camera.transform.eulerAngles = new Vector3(30f, 0f, 0f);
+        
+        ballLine.SetActive(true);
     }
 }
